9c2d0b2be5a61c238dc003ae64b603a15a2d71f7
G.bhavyasree <234g1a331@srit.ac.in>
2025-09-11 13:55:39 +0530

    commit

--- frontend/new1.js
+++ frontend/new1.js
@@ -4,7 +4,7 @@ let num = 12;
 let str = "hello";
 let bol = true;
 const bigIntVal = 1234567890123456789012345678901234567890n;
-let undef; 
+let undef;
 let nul = null;
 const sym = Symbol("id");
 
@@ -16,26 +16,53 @@ console.log(undef);
 console.log(nul);
 console.log(sym);
 
-let x = 5;
-let y = 6;
-let z = x + y;
+x = 5;
+y = 6;
+z = x + y;
 console.log(z);
 
-const price1 = 5;
+const price1 = 35;
 const price2 = 6;
-let total = price1 + price2;
+total = price1 + price2;
 console.log(total);
 
-let name1 = "John"; // Use let instead of const
+name1 = "John";
 console.log(name1);
 
-name1 = "Doe"; 
+name1 = "Doe";
 console.log(name1);
 
 // Function in JavaScript
-function detailsofstudent(name, rollNo, age) {
-    return "Name: " + name + ", Roll No: " + rollNo + ", Age: " + age;
+
+
+function detailsofstudent(name, rollNo, age, score) {
+    return "Name: " + name + ", Roll No: " + rollNo + ", Age: " + age + ", Score: " + score;
 }
 
-let studentName = detailsofstudent('Bhavya', '234g1a3319', 20);
+let studentName = detailsofstudent('Bhavya', '234g1a3319', 20, 90);
 console.log(studentName);
+
+let score = 90;
+let addscore = score + 3;
+console.log("Updated Score:", addscore);
+
+function areaOfRectangle(length, width) {
+    return length * width;
+}
+
+let length = 10;
+let width = 5;
+let area = areaOfRectangle(length, width);
+console.log("Area of Rectangle:", area);
+
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+let number = 100; 
+let result = factorial(number);
+console.log("Factorial of " + number + " is " + result);
